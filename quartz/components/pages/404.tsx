c581f567b52c19e7c4435ff76239bfc2aa62f51b
danielsuss <captainrelic123@gmail.com>
2024-04-19 11:57:35 +0200

    Quartz sync: Apr 19, 2024, 11:57 AM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -1,20 +1,23 @@
 import { i18n } from "../../i18n"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
-const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
-  return (
-    <>
-      <div class="norbert">
-        <img class="norbertImg" src="/attachments/Norbert.webp"></img>
-      </div>
-      <div class="norbert">
-        <p>Norbert here, explore some more to find this lore...</p>
-      </div>
-      <div class="norbert">
-        <a href="https://hexacordia.pages.dev/" class="internal">Home</a>
-      </div>
-    </>
-  )
-}
+export default (() => {
+  const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
+    return (
+      <>
+        <div class="norbert">
+          <img class="norbertImg" src="/attachments/Norbert.webp"></img>
+        </div>
+        <div class="norbert">
+          <p>Norbert here, explore some more to find this lore...</p>
+        </div>
+        <div class="norbert">
+          <button class="backBtn" id="back" onclick="window.history.back();">Back</button>
+        </div>
+      </>
+    )
+  }
 
-export default (() => NotFound) satisfies QuartzComponentConstructor
+  return NotFound;
+
+}) satisfies QuartzComponentConstructor
