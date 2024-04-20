c9c8fec4506e19dfe115172abc999c9823d201b9
danielsuss <captainrelic123@gmail.com>
2024-04-20 03:02:55 +0200

    Quartz sync: Apr 20, 2024, 3:02 AM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -9,7 +9,7 @@ export default (() => {
           <img class="norbertImg" src="/attachments/Norbert.webp"></img>
         </div>
         <div class="norbert">
-          <p>Norbert here, this lore's still in the oven. Check back later...</p>
+          <p>Norbert here, this piece of lore's still in the oven. Check back later...</p>
         </div>
         <div class="norbert">
           <button class="backBtn" id="back" onclick="window.history.back();">Back</button>
